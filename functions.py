0821ca609e49a435e5c4045aeed47acb86a08773
Mauricio García <nagmjgg@yahoo.com>
2022-03-16 17:01:59 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -8,7 +8,6 @@ import os
 import math
 from tkcalendar import Calendar
 
-
 def find_last_file(folder, initials):  # folder: 'D:/shared_inventory/server files/', initials: 'available'
     location = folder + initials
     list_of_files = glob.glob(location + '*.csv')  # * means all if need specific format then *.csv
@@ -135,17 +134,119 @@ def validate_if_empty_text(widget):
         messagebox.showerror(title="Warning", message="Can't be empty or text")
         return 0
 
-def load_db_data(table):
-    data = pd.read_sql('select * from ' + table, conn)
+def load_db_data(table, db_connection):
+    data = pd.read_sql('select * from ' + table, db_connection)
     return data
 
 def load_csv_data(dataframe):
     folder = "D:/shared_inventory/server files/"
     filename = find_last_file(folder, dataframe)
     print(filename)
-    data = pd.read_csv(filename, index_col=False, encoding='latin-1')
+    data = pd.read_csv(filename, index_col='id', encoding='latin-1')
     return data
 
-def delete_table(table):
-    query = "delete from " + table
-    cursor.execute(query)
+def dataframe_to_dictionary(csv_file_initials):
+    # load data from csv file
+    df_data = load_csv_data(csv_file_initials)
+    df_data = df_data.to_dict()
+    return df_data
+
+def dataframe_columns(csv_file_initials):
+    df_data = load_csv_data(csv_file_initials)
+    df_data = df_data.to_dict()
+    columns = list(df_data.keys())
+    #print(f"columns: {columns}")
+    return columns
+
+def dataframe_columns_count(csv_file_initials):
+    df_data = load_csv_data(csv_file_initials)
+    df_data = df_data.to_dict()
+    columns = list(df_data.keys())
+    #print(f"columns: {columns}")
+    columns_len = len(columns)
+    #print(f"columns length: {columns_len}")
+
+def dataframe_rows_count(csv_file_initials):
+    df_data = load_csv_data(csv_file_initials)
+    df_data = df_data.to_dict()
+    columns = list(df_data.keys())
+    rows = len(df_data[columns[0]])
+    return rows
+
+def create_insert_query_for_table(db_dataframe, database_table):
+    # this function works together with create_cursor_insert_query_for_table() as a complement to create the next sentences
+    #     #query = "insert into parts (col1, col2, col3, ...) values (%s, %s, %s, %s, ... )"
+    #     #cursor.execute(query, (%s,%s,%s,%s...))
+    # it takes the database columns for creating columns in query
+
+    print(f"<<<  Text for query ok...>>>")
+
+    columns = db_dataframe.columns
+    columns_len = len(columns)
+
+    first_db_column = columns[0]
+
+    rows = len(db_dataframe[first_db_column])
+    print(f" len {len(db_dataframe[first_db_column])}")
+
+    text1 = "insert into "
+
+    text2 = " ("
+
+    query_columns = ""
+    values_symbol = ""
+
+    count = 0
+    for i in columns:
+        query_columns = query_columns + i
+        values_symbol = values_symbol + "%s"
+        if count < (columns_len - 1):
+            query_columns += ", "
+            values_symbol += ", "
+            count += 1
+        else:
+            query_columns += ") "
+            values_symbol += ")"
+            count += 1
+
+    text3 = " values ("
+    #print(query_columns)
+
+    text_columns = query_columns
+    query = text1 + database_table + text2 + text_columns + text3 + values_symbol
+    return query
+
+def create_cursor_insert_query_for_table(csv_dataframe, dataframe_name, table):
+    #this function works together with create_insert_query_for_table() as a complement to create the next sentences
+    #query = "insert into parts (col1, col2, col3, ...) values (%s, %s, %s, %s, ... )"
+    #cursor.execute(query, (%s,%s,%s,%s...))
+
+    print(f"<<< Cursor insert text for query ok...>>>")
+
+    # load data from csv file
+    columns = csv_dataframe.columns
+
+    # columns count
+    columns_len = len(columns)
+    #print(f"columns length: {columns_len}")
+
+    #first database column
+    first_db_column = columns[0]
+
+    #rows count
+    rows = len(csv_dataframe[first_db_column])
+    #print(f" len {len(df_data[first_db_column])}")
+
+    cursor_columns = ""
+    cursor = ""
+    count = 0
+    for i in columns:
+        cursor_columns += dataframe_name +"['" + i + "'][i]"
+        if count < (columns_len - 1):
+            cursor_columns += ", "
+            count += 1
+        else:
+            cursor_columns += ""
+            count += 1
+
+    return cursor_columns
